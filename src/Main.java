963aebc6f83bb236d32b7d473fc8f52ab5297688
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-02-20 16:22:02 +0500

    added getting data from the terminal

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    private final static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        int[] array = fillArray(10, 1, 20);
+        int[] array = fillArray(getLengthTerminal(), getMinValueTerminal(), getMaxValueTerminal());
         System.out.println(Arrays.toString(array));
     }
 
@@ -14,5 +16,18 @@ public class Main {
             array[i] = rnd.nextInt((max - min) +1) + min;
         }
         return array;
+    }
+
+    private static int getLengthTerminal(){
+        System.out.print("Enter length of array: ");
+        return scan.nextInt();
+    }
+    private static int getMinValueTerminal(){
+        System.out.print("Enter min value of array: ");
+        return scan.nextInt();
+    }
+    private static int getMaxValueTerminal(){
+        System.out.print("Enter max value of array: ");
+        return scan.nextInt();
     }
 }
