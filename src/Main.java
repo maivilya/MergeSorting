7846155c34ee39eb59d4ca0c0d60f46705907a23
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-02-20 16:16:10 +0500

    added fill array method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = fillArray(10, 1, 20);
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static int[] fillArray(int length, int min, int max){
+        int[] array = new int[length];
+        Random rnd = new Random();
+        for (int i = 0; i <array.length; i++){
+            array[i] = rnd.nextInt((max - min) +1) + min;
+        }
+        return array;
     }
 }
