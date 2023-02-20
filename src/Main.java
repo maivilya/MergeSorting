06900eeb1496a8ba86c1acc47d8f48e49517aa17
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-02-20 17:52:38 +0500

    added merge and mergeSort methods

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,15 @@ public class Main {
     private final static Scanner scan = new Scanner(System.in);
     private final static String inputFile = "src/input.txt";
 
-    public static void main(String[] args) throws IOException {
-
-
+    public static void main(String[] args) throws IOException{
     }
 
-    private static String toString(int[] array){
+    private static String toString(int[] array) {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         int length = array.length;
-        for(int i = 0; i < length; i++){
-            if (i < (length - 1)){
+        for (int i = 0; i < length; i++) {
+            if (i < (length - 1)) {
                 sb.append(String.format("%d, ", array[i]));
             } else {
                 sb.append(String.format("%d]", array[i]));
@@ -28,6 +26,57 @@ public class Main {
         }
         return sb.toString();
     }
+
+    private static void merge(int[] src1, int src1Start, int[] src2, int src2Start,
+                              int[] dest, int destStart, int size) {
+
+        int index1 = src1Start;
+        int index2 = src2Start;
+
+        int src1End = Math.min(src1Start + size, src1.length);
+        int src2End = Math.min(src2Start + size, src2.length);
+
+        if (src1Start + size > src1.length) {
+            for (int i = src1Start; i < src1End; i++) {
+                dest[i] = src1[i];
+            }
+            return;
+        }
+
+        int iterationCount = src1End - src1Start + src2End - src2Start;
+
+        for (int i = destStart; i < destStart + iterationCount; i++) {
+            if (index1 < src1End && (index2 >= src2End || src1[index1] < src2[index2])) {
+                dest[i] = src1[index1];
+                index1++;
+            } else {
+                dest[i] = src2[index2];
+                index2++;
+            }
+        }
+    }
+
+    private static int[] mergeSort(int[] array) {
+        int[] temp;
+        int[] currentSrc = array;
+        int[] currentDest = new int[array.length];
+
+        int length = array.length;
+        int size = 1;
+        while (size < length) {
+            for (int i = 0; i < length; i += size * 2) {
+                merge(currentSrc, i, currentSrc, i + size, currentDest, i, size);
+            }
+
+            temp = currentSrc;
+            currentSrc = currentDest;
+            currentDest = temp;
+
+            size *= 2;
+        }
+        return currentSrc;
+    }
+
 
     private static int getDataFromFile(String inputFileName, String value) throws IOException {
         String tempResult = "0";
